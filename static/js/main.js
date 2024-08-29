8a2ddf3278f69df78bc1b6eee87b95a6faea7eb0
鹊楠 <11601350+quenan666@user.noreply.gitee.com>
2024-08-29 23:49:26 +0800

    Site updated: 2024-08-29 23:49:25

--- static/js/main.js
+++ static/js/main.js
@@ -70,33 +70,3 @@ window.addEventListener('scroll', function () {
 // 首页封面样式 end --
 
 
-
-// 初始状态设置
-document.querySelectorAll('.details').forEach((details) => {
-    if (details.hasAttribute('open')) {
-        details.classList.add('active');
-        const content = details.querySelector('.content');
-        content.style.maxHeight = content.scrollHeight + "px";
-    }
-});
-
-// 添加点击事件
-document.querySelectorAll('.collapsible').forEach((button) => {
-    button.addEventListener('click', function () {
-        const details = this.parentElement;
-        toggleDetails(details);
-    });
-});
-
-function toggleDetails(details) {
-    details.classList.toggle('active');
-    const content = details.querySelector('.content');
-
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-    } else {
-        content.style.maxHeight = (content.scrollHeight + 20) + "px";
-    }
-}
-
-
